54fb2080cb72f2e278ff65d01c7ce6ac6a8bbd84
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2024-07-24 21:33:13 +0530

    Update 4_OOPs.py

--- 4_OOPs.py
+++ 4_OOPs.py
@@ -4,27 +4,27 @@ Ex :-
 """
 
 
-# class Car:
-#     def __init__(self, type):
-#         self.type = type
-#
-#     @staticmethod
-#     def start():
-#         print("car started...")
-#
-#     @staticmethod
-#     def stop():
-#         print("car stopped...")
-#
-#
-# class ToyotaCar(Car):
-#     def __init__(self, name, type):
-#         self.name = name
-#         super().__init__(type)
-#
-#
-# car1 = ToyotaCar("prius", "electric")
-# print(car1.type)
+class Car:
+    def __init__(self, type):
+        self.type = type
+
+    @staticmethod
+    def start():
+        print("car started...")
+
+    @staticmethod
+    def stop():
+        print("car stopped...")
+
+
+class ToyotaCar(Car):
+    def __init__(self, name, type):
+        self.name = name
+        super().__init__(type)
+
+
+car1 = ToyotaCar("prius", "electric")
+print(car1.type)
 
 
 """
@@ -40,31 +40,31 @@ Ex :-
 """
 
 
-# class Person:
-#     name = "anonymous"
-#
-#     def ChangeName(self, name):
-#         self.__class__.name = "Rahul"
-#
-#
-# p1 = Person()
-# p1.ChangeName("rahul kumar")
-# print(p1.name)
-# print(Person.name)
+class Person:
+    name = "anonymous"
+
+    def ChangeName(self, name):
+        self.__class__.name = "Rahul"
 
 
-# class Person:
-#     name = "Anonymous"
-#
-#     @classmethod
-#     def ChangeName(cls, name):
-#         cls.name = name
-#
-#
-# p1 = Person()
-# p1.ChangeName("Rahul kumar")
-# print(p1.name)
-# print(Person.name)
+p1 = Person()
+p1.ChangeName("rahul kumar")
+print(p1.name)
+print(Person.name)
+
+
+class Person:
+    name = "Anonymous"
+
+    @classmethod
+    def ChangeName(cls, name):
+        cls.name = name
+
+
+p1 = Person()
+p1.ChangeName("Rahul kumar")
+print(p1.name)
+print(Person.name)
 
 """
 Property :- we use property decorator on any method in the class to use the method as a property.
@@ -72,32 +72,32 @@ Ex:-
 """
 
 
-# class Student:
-#     def __init__(self, phy, chem, math):
-#         self.phy = phy
-#         self.math = math
-#         self.chem = chem
-#
-#     # def calcPercentage(self):
-#     #     self.percentage = str((self.phy + self.chem + self.math) / 3) + "%"
-#         @property
-#         def percentage(self):
-#             return str((self.phy + self.chem + self.math) / 3) + "%"
-#
-#
-# std1 = Student(98, 97, 96)
-# print(std1.percentage())
-# std1.phy = 86
-# # print(calcpercentage())
-# print(std1.percentage())
+class Student:
+    def __init__(self, phy, chem, math):
+        self.phy = phy
+        self.math = math
+        self.chem = chem
+
+    # def calcPercentage(self):
+    #     self.percentage = str((self.phy + self.chem + self.math) / 3) + "%"
+        @property
+        def percentage(self):
+            return str((self.phy + self.chem + self.math) / 3) + "%"
+
+
+std1 = Student(98, 97, 96)
+print(std1.percentage())
+std1.phy = 86
+# print(calcpercentage())
+print(std1.percentage())
 
 """
 Polymorphism :- Operator Overloading :- when the same operator is allowed to have different meaning according to the context.
 Ex :-  
 """
-# print(2+3) #3
-# print("apna" + "college") # apna college
-# print([1,2 3] + [4, 5, 6]) # merge
+print(2+3) #3
+print("apna" + "college") # apna college
+print([1,2 3] + [4, 5, 6]) # merge
 
 """
 Operator & Dunder Functions -
@@ -110,23 +110,23 @@ Ex :-
 """
 
 
-# class Complex :
-#     def __init__(self, real, img):
-#         self.real = real
-#         self.img = img
-#
-#     def showNumber(self):
-#         print(self.real, "i+", self.img, "j")
-#
-#     def add(self, num2):
-#         newReal = self.real + num2.real
-#         newImg = self.img + num2.img
-#         return Complex(newReal, newImg)
-#
-#
-# num1 = Complex(1, 3)
-# num1.showNumber()
-# num2 = Complex(4, 6)
-# num2.showNumber()
-# num3 = num1.add(num2)
-# num3.showNumber()
+class Complex :
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+
+    def showNumber(self):
+        print(self.real, "i+", self.img, "j")
+
+    def add(self, num2):
+        newReal = self.real + num2.real
+        newImg = self.img + num2.img
+        return Complex(newReal, newImg)
+
+
+num1 = Complex(1, 3)
+num1.showNumber()
+num2 = Complex(4, 6)
+num2.showNumber()
+num3 = num1.add(num2)
+num3.showNumber()
